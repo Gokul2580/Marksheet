62c97d96415334e094ae44f43aae8edb9e5b5a92
Gokul <143377276+Gokul2580@users.noreply.github.com>
2025-04-11 09:07:46 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-apt-get update && apt-get install -y tesseract-ocr
+
+# Install Tesseract OCR
+sudo apt-get update
+sudo apt-get install -y tesseract-ocr
+
+# Install Python dependencies
+pip install -r requirements.txt
